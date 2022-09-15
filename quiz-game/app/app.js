68478f1fa53775705304d1748dad61d3367bab95
samir <smairhguerrero@email.com>
2022-09-15 10:10:34 -0500

    third commit

---
+++ quiz-game/app/app.js
@@ -0,0 +1,11 @@
+const question = document.querySelector('.question'), 
+		numberOfQuestion = document.querySelector('.question__number'),
+		numberOfAllQuestions = document.querySelector('.question__all-numbers')
+;
+
+const options = document.querySelectorAll('.answers__option');
+const option_1 = document.querySelector('.answers__option-1'), 
+      option_2 = document.querySelector('.answers__option-2'), 
+      option_3 = document.querySelector('.answers__option-3'), 
+      option_4 = document.querySelector('.answers__option-4')
+;
